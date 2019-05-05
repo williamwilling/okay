141defe5bb6262decdc2835268958aa04d84288e
Joost Ronkes Agerbeek <joost.ronkes@trivago.com>
2019-05-05 14:48:31 +0200

    You can specify required top-level fields.

--- src/validator.py
+++ src/validator.py
@@ -1,6 +1,25 @@
+class Message:
+    def __init__(self, type, field):
+        self.type = type
+        self.field = field
+
 class Validator:
     def __init__(self, schema):
-        pass
+        self._schema = schema
+        self.messages = []
     
     def validate(self, document):
-        return True
+        if not isinstance(document, dict):
+            raise TypeError('Document must be dictionary.')
+        
+        self._document = document
+        self._schema(self)
+
+        return len(self.messages) == 0
+    
+    def required(self, field):
+        if not field in self._document:
+            self.messages.append(Message(
+                type='missing_field',
+                field=field
+            ))
--- tests/test_validator.py
+++ tests/test_validator.py
@@ -1,12 +1,44 @@
+import pytest
 from validator import Validator
 
 class TestValidator:
-    def test_accepts_any_document_when_the_schema_is_empty(self):
+    def test_it_accepts_any_document_when_the_schema_is_empty(self):
+        document = {}
+        validator = Validator(empty_schema)
+        is_valid = validator.validate(document)
+
+        assert is_valid
+    
+    def test_it_raises_when_document_is_not_a_dictionary(self):
+        document = ''
+        validator = Validator(empty_schema)
+        with pytest.raises(TypeError):
+            validator.validate(document)
+    
+    def test_it_reports_a_missing_required_top_level_field(self):
         def schema(validator):
-            pass
-        
+            validator.required('metadata')
+
         document = {}
         validator = Validator(schema)
         is_valid = validator.validate(document)
 
+        assert not is_valid
+        message = validator.messages[0]
+        assert message.type == 'missing_field'
+        assert message.field == 'metadata'
+    
+    def test_it_accepts_a_required_top_level_field(self):
+        def schema(validator):
+            validator.required('metadata')
+        
+        document = { 'metadata': True }
+        validator = Validator(schema)
+        is_valid = validator.validate(document)
+
         assert is_valid
+        assert validator.messages == []
+
+
+def empty_schema(validator):
+    pass
